a070d90d231df63e556ab3e6971702bff0b422cb
GitHubmandan850 <113126883+GitHubmandan850@users.noreply.github.com>
2022-10-19 08:45:32 -0700

    E

--- CreateWithCode/Protoype 2/Assets/Scripts/spawnManager.cs
+++ CreateWithCode/Protoype 2/Assets/Scripts/spawnManager.cs
@@ -7,22 +7,26 @@ public class spawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeZ = 20;
     private float SpawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnIntervole = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnIntervole);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S)) 
-        {
-           int animalIndex = Random.Range(0, animalPrefabs.Length);
-           Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeZ, spawnRangeZ), 0 ,SpawnPosZ);
+       
+    }
 
-           Instantiate(animalPrefabs[animalIndex], spawnpos, animalPrefabs[animalIndex].transform.rotation);
-        }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeZ, spawnRangeZ), 0 ,SpawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnpos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
