7a8905f91dd870bf7185b7fef0444e7f1b0608d1
GitHubmandan850 <113126883+GitHubmandan850@users.noreply.github.com>
2022-10-25 09:14:31 -0700

    W

--- CreateWithCode/Protoype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ CreateWithCode/Protoype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,8 +10,8 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float startDelay = 3.0f;
+    private float spawnInterval = 5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +23,11 @@ public class SpawnManagerX : MonoBehaviour
     void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
