d2d6caf84b40463d2cbc38c305a963c5b1a17114
GitHubmandan850 <113126883+GitHubmandan850@users.noreply.github.com>
2023-02-17 09:12:42 -0800

    heheheha

---
+++ CreateWithCode/Potential arcade game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    public float health = 100;
+    public float speed = 5.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    
+    private string inputID; 
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.left * Time.deltaTime * speed * horizontalInput);
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 10.0f;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+         speed = 5.0f;
+        }
+
+        health = Mathf.Clamp(health, 0, 100);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(health < 0)
+        {
+            Destroy(player.gameObject);
+            speed = 0;
+        }
+    }
+}
+
